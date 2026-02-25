e0790631dd73ef1d843b861aa999cf7e6ca1a7de
Loaching <xbd77fuz@qq.com>
2026-02-25 17:27:03 +0800

    Create  图的存储.cpp

---
+++ Solutions/图的存储.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m;
+vector<int>G[1005];
+int a[1005][1005],de[1005];
+int main(){
+	cin>>n>>m;
+	for(int i=1,u,v;i<=m;i++){
+		cin>>u>>v;
+		G[u].push_back(v);
+		G[v].push_back(u);
+		a[u][v]=1;
+		a[v][u]=1;
+		de[u]++;
+		de[v]++;
+	}
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			cout<<a[i][j]<<' ';
+		}
+		cout<<'\n';
+	}
+	for(int i=1;i<=n;i++){
+		cout<<de[i]<<' ';
+		sort(G[i].begin(),G[i].end());
+		for(int j=0;j<de[i];j++)	cout<<G[i][j]<<' ';
+		cout<<'\n';
+	} 
+	return 0;
+}
+/*
+https://www.luogu.com.cn/problem/B3643
+弱智题
+*/
