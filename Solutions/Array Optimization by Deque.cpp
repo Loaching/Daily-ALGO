73e2d840698b4224a03cf50ada336aba76addf9e
Loaching <xbd77fuz@qq.com>
2026-04-17 23:51:13 +0800

    Update Array Optimization by Deque.cpp

--- Solutions/Array Optimization by Deque.cpp
+++ Solutions/Array Optimization by Deque.cpp
@@ -60,4 +60,26 @@ signed main() {
 /*
 https://codeforces.com/contest/1579/problem/E2
 https://www.luogu.com.cn/problem/CF1579E2
+
+学着点，离散化
+
+先排序，因为unique需保证有序
+sort(all(b));
+
+unique(b.begin(),b.end())返回去重后所余最后一个元素的迭代器
+e.g.
+b={1, 1, 2, 3, 3, 3, 3, 4, 5};
+*unique(b.begin(),b.end())=5;
+此时b={1, 2, 3, 4, 5, 1, 3, 3}
+                         ^返回这个指针
+
+删除
+b.erase(unique(all(b)), b.end());
+
+将原数组按照在b内的排序收缩值域，完成离散化
+也可以开个新数组，再用b作为一个离散化后的值到源数据间的映射
+具体在Moving Points此题中有
+for (int i = 0; i < n; i++)	a[i] = lower_bound(all(b), a[i]) - b.begin() + 1;
+
+
 */
